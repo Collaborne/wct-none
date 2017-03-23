46a5980146319647bc51adfb4896cc62a8b5fed7
Andreas Kohn <andreas.kohn@gmail.com>
2017-03-23 11:30:58 +0100

    Tell the user to ignore ECONNREFUSED 127.0.0.1:4444
    Related-to: #1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ module.exports = function(context, pluginOptions, plugin) {
 	// Reset the browsers to just contain dummy browser, so that wct doesn't complain about no browsers at all.
 	context.hook('configure', function(done) {
 		context.options.activeBrowsers = [{}];
+		// FIXME: Obviously, fix the error. Probably by exposing a server on that port and accepting the connection.
+		console.log('Configured dummy browser. An error about ECONNREFUSED to 127.0.0.1:4444 can be ignored');
 		return done(null);
 	});
 };
